3a33f0c21530a87a38a62a499b955c89925b38d6
Zietieflr <lrmcguire93@gmail.com>
2020-05-13 16:22:32 -0600

    Cleaned and organized code

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,89 +1,61 @@
 require 'pry'; 
 
-#Helper Method => Returns the season array after given the entire data structure and season. 
-def find_correct_season (data, season)
-  data.select { |data| 
-  data.to_s === season; 
-}
-end 
 
-def collect_contestants (data)
-  all_contestants = []; 
-  data.map { |season, contestants| 
+
+def find_correct_season (data, season_query)
+  data.filter { |season, contestant| 
+    season === season_query; 
+  }
+end  
+
+def collect_contestants (received_data)
+  contestants_by_season = received_data.map { |season, contestants| 
     contestants.map { |contestant| 
       contestant["season"] = season;
-      all_contestants << contestant; 
+      contestant; 
     }
   }
-  all_contestants; 
+  contestants_by_season.reduce([]) { |collect, contestant|
+    collect.concat contestant
+  }
 end 
 
 def winner_info (data, season)
-  season_array = find_correct_season(data, season); 
-  season_array[season].select { |contestant| 
+  find_correct_season(data, season)[season].find { |contestant| 
     contestant["status"] === "Winner"; 
-  } 
+  }  
 end 
 
 def get_first_name_of_season_winner(data, season)
-  name = winner_info(data, season)[0]["name"]; 
-  name.split[0]; 
+  name = winner_info(data, season)["name"];
+  (name.split).first;   
 end
 
 def get_contestant_name(data, occupation)
-  contestant = collect_contestants(data).select { |contestant|
+  contestant = collect_contestants(data).find { |contestant|
     contestant["occupation"] === occupation;
   }
-  contestant[0]["name"];
+  contestant["name"];
 end
 
-# def count_contestants_by_hometown(data, hometown)
-#   trial = collect_contestants(data).reduce(0) {|count, contestant|
-#   #binding.pry; 
-#     if contestant["hometown"] === hometown
-#       #count += 1; 
-#       binding.pry;
-#     end 
-#   }
-#   binding.pry; 
-#   trial; 
-# end
-
 def count_contestants_by_hometown(data, hometown)
-  count = 0; 
-  collect_contestants(data).map {|contestant|
-  #binding.pry; 
-    if contestant["hometown"] === hometown
-      count += 1; 
-    end 
+  collect_contestants(data).reduce(0) { |count, contestant|
+    contestant["hometown"] === hometown ? count += 1 : count += 0
   }
-  count; 
 end
 
 def get_occupation(data, hometown)
-  contestant = collect_contestants(data).select { |contestant|
+  contestant_info = collect_contestants(data).find { |contestant|
     contestant["hometown"] === hometown;
   }
-  contestant[0]["occupation"];
+  contestant_info["occupation"];
 end
 
-# def get_average_age_for_season(data, season)
-#   ages = find_correct_season(data, season)[season].reduce([age: 0, count: 0]) {|memo, contestant|
-#     binding.pry; 
-#     memo[0][:age] += contestant["age"].to_i;
-#     memo[0][:count] += 0;
-  
-#     binding.pry; 
-#   }
-# ages
-# binding.pry; 
-# end
-
 def get_average_age_for_season(data, season)
-  contest_age = {age: 0, count: 0};
-  find_correct_season(data, season)[season].map {|contestant| 
-    contest_age[:age] += contestant["age"].to_i;
-    contest_age[:count] += 1;
+  contest_age = find_correct_season(data, season)[season].reduce({age: 0, count: 0}) {|age_and_count, contestant| 
+    age_and_count[:age] += contestant["age"].to_i;
+    age_and_count[:count] += 1;
+    age_and_count 
   }
 (contest_age[:age]/contest_age[:count].to_f).round(); 
 #For my own understanding => .to_f converts to float, which stores the 19/25 lost from Integers, then rounds
